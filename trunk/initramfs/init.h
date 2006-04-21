76950bb6737ff8540d5de9c80a9aeae06423362b
jhuntwork <jhuntwork@d8068324-2beb-0310-80ce-8d62b0f71719>
2006-04-21 04:23:12 +0000

    Merged udev_update branch to trunk.

--- trunk/initramfs/init.h
+++ trunk/initramfs/init.h
@@ -13,18 +13,14 @@
 #include <linux/cdrom.h>
 #include <linux/fs.h>
 #include <sys/reboot.h>
+#include <libdevmapper.h>
 #include "iso9660.h"
 
 #define TMPFS		"/.tmpfs"		  	/* Mount point for tmpfs */
 #define CDROM_MOUNT	"/.tmpfs/.cdrom"		/* Mount point for CD */
-#define SQFS		"/.tmpfs/.sqfs"			/* Mount point for squashfs */
-#define SQFS_FILE	"/.tmpfs/.cdrom/.root.sqfs"	/* Squashfs file, root system */
+#define ROOT_FILE	"/.tmpfs/.cdrom/root.ext2"	/* Origin file for the root fs */
+#define ROOT		"/.root"			/* Mount point for the root fs */
 #define OVERLAY		"/.tmpfs/.overlay"		/* Full path to overlay */
-#define SHM		"/.tmpfs/.overlay/dev/shm"	/* Full path to dev/shm */
-#define PROC		"/.tmpfs/.overlay/proc"		/* Full path to proc */
-#define DEV		"/.tmpfs/.overlay/dev"		/* Full path to dev */
-#define TMP		"/.tmpfs/.overlay/tmp"		/* Full path to tmp */
-#define LOOP		"/dev/loop0"			/* Loopback device */
 
 #define	ISO_BLOCK_SIZE	2048
 #define	ISO_PD_BLOCK	0x10
---
+++ trunk/packages/udev/bug.c
@@ -0,0 +1,26 @@
+/* Simple event recorder */
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <argz.h>
+
+int main(int argc, char * argv[])
+{
+	char * envz;
+	size_t len;
+	int bug;
+	bug = open("/dev/bug", O_WRONLY | O_APPEND);
+	if (bug == -1)
+		return 0;
+	setenv("_SEPARATOR", "--------------------------------------", 1);
+	argz_create(environ, &envz, &len);
+	argz_stringify(envz, len, '\n');
+	envz[len-1]='\n';
+	write(bug, envz, len);
+	close(bug);
+	free(envz);
+	return 0;
+}
