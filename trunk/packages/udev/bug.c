422bc1b6e073d300ac566b738883051aec936002
alexander <alexander@d8068324-2beb-0310-80ce-8d62b0f71719>
2006-05-03 13:47:35 +0000

    Updated udev and LFS book

--- trunk/packages/udev/bug.c
+++ trunk/packages/udev/bug.c
@@ -9,12 +9,22 @@
 
 int main(int argc, char * argv[])
 {
+	char * envar;
 	char * envz;
 	size_t len;
 	int bug;
 	bug = open("/dev/bug", O_WRONLY | O_APPEND);
 	if (bug == -1)
 		return 0;
+
+	/* Ignore everything USB-related to avoid spamming the list */
+	envar = getenv("PHYSDEVPATH");
+	if (envar && strstr(envar, "usb"))
+		return 0;
+	envar = getenv("DEVPATH");
+	if (envar && strstr(envar, "usb"))
+		return 0;
+	
 	setenv("_SEPARATOR", "--------------------------------------", 1);
 	argz_create(environ, &envz, &len);
 	argz_stringify(envz, len, '\n');
